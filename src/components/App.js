2634e210939695e1804e3d0a2a22c50888bc46a6
beurkinger <tgoehringer@gmail.com>
2017-06-19 13:23:34 +0200

    css & html update

--- src/components/App.js
+++ src/components/App.js
@@ -5,24 +5,25 @@ class App extends Component {
 
   constructor(props) {
 		super(props);
+    this.state = {str : "SOME CUMMIES"};
 	}
 
 	render () {
 		return (
       <div id="app">
-        <svg id="svg-box" viewBox="0 0 100 100">
+        <svg id="svg-box" viewBox="0 0 40 40">
           <title>Test</title>
           <g id="text-container">
-            <g transform="translate(0.4, 0.4)">
+            <g transform="translate(0.45, 0.45)">
                 <text id="text-shadow" x="50%" y="50%">
-                  TWIN PEAKS
+                  {this.state.str}
                 </text>
             </g>
             <text id="text1" x="50%" y="50%">
-              TWIN PEAKS
+              {this.state.str}
             </text>
             <text id="text2" x="50%" y="50%">
-              TWIN PEAKS
+              {this.state.str}
             </text>
           </g>
         </svg>
